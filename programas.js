fb09b262d12e5351040de1035d4eed82b7b57674
victor samuel medina trujillo <mt202516593@alm.buap.mx>
2026-08-26 18:19:05 -0600

    Add files via upload

---
+++ programas.js
@@ -0,0 +1,39 @@
+const PROGRAMAS_TDH = {
+
+    CREADOR_DIGITAL: {
+
+        nombre: "Creador Digital TDH",
+
+        codigo: "CD",
+
+        generacion: "1GEN-2026",
+
+        modalidad: "100% Online",
+
+        horas: 24,
+
+        edad: "8 a 17 años",
+
+        materias: {
+
+            "IA para niños y jóvenes": {
+                codigo: "IA"
+            },
+
+            "Algoritmos y pensamiento lógico": {
+                codigo: "ALGO"
+            },
+
+            "Ciudadanía digital": {
+                codigo: "CIUD"
+            },
+
+            "Emprendimiento digital": {
+                codigo: "EMP"
+            }
+
+        }
+
+    }
+
+};
